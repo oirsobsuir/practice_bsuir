a5dcd42d696c52a386bfc364183ddca65299afab
samaragang <dmitr.burii.2016@gmail.com>
2023-06-12 13:28:25 +0300

    height fix

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-console.log("Ширина окна: " + window.innerWidth);
-console.log("Высота окна: " + window.innerHeight);
+let width = window.innerWidth;
+let height = window.innerHeight;
+let divWidth = document.getElementById('width');
+let divHeight = document.getElementById('height');
+
+divWidth.innerHTML = width;
+divHeight.innerHTML = height;

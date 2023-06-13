4efa30ad7cc4dea6c46a9f601c9eb487b4d491e8
samaragang <dmitr.burii.2016@gmail.com>
2023-06-13 11:48:13 +0300

    final

--- script.js
+++
@@ -1,7 +0,0 @@
-let width = window.innerWidth;
-let height = window.innerHeight;
-let divWidth = document.getElementById('width');
-let divHeight = document.getElementById('height');
-
-divWidth.innerHTML = width;
-divHeight.innerHTML = height;
